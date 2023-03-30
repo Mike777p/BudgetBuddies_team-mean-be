8055766598382d3a2138c9ea644b93bf69190c2d
Ell534 <elliot94b@gmail.com>
2023-03-30 14:07:14 +0100

    add GET request to retrieve a specific users balance

--- Models.js/UserModels.js
+++ Models.js/UserModels.js
@@ -1,15 +1,25 @@
 const User = require('../db/seeds/models.js/Users');
-
+const Budget = require('../db/seeds/models.js/BudgetModel');
 
 const fetchUsers = async () => {
-    try {
-        const users = await User.find();
-        return users;
-      } catch (error) {
-        console.error(error);
-        throw error;
-      }
-}
+  try {
+    const users = await User.find();
+    return users;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
+const fetchUserBalance = async (user_id) => {
+  try {
+    const userBalance = await Budget.find({ user_id }, { user_id: 1, balance: 1 });
+    // this is saying find where user_id = user_id and only give me balance, if it was balance : 0, this would omit balance from the results, can also do .find({user_id}).select('balance')
+    return userBalance;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
-module.exports = fetchUsers;
+module.exports = { fetchUsers, fetchUserBalance };
--- server.js
+++ server.js
@@ -1,33 +1,37 @@
 const express = require('express');
 const app = express();
-const dotenv = require("dotenv");
-const connectDB = require("./connect.js");
-
+const dotenv = require('dotenv');
+const connectDB = require('./connect.js');
 
 app.use(express.json());
 
-dotenv.config()
+dotenv.config();
 
-const getUsers = require('./Controllers/UserControllers.js');
+const {
+  getUsers,
+  getUserBalance,
+} = require('./Controllers/UserControllers.js');
 
 // This route can be changed / used
-app.get("/", (req, res)=>{
-    res.send("Welcome to Budget Buddies");
-})
+app.get('/', (req, res) => {
+  res.send('Welcome to Budget Buddies');
+});
 
-app.get("/users", getUsers);
+app.get('/users', getUsers);
+
+app.get('/users/:user_id/balance', getUserBalance);
 
 const port = process.env.PORT || 5000;
 
 const start = async () => {
-    try {
-     await connectDB(process.env.MONGO_URL)
-     app.listen(port, () => {
-        console.log(`Server is listening on port ${port}...`);
-      });
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-start()
+start();
