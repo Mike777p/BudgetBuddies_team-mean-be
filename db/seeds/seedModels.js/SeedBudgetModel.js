6b6532b437aa33c2d480263063dab19b31d16809
ahmedmassli <ahmedmassli@hotmail.co.uk>
2023-04-03 20:29:48 +0100

    last monday

--- db/seeds/seedModels.js/SeedBudgetModel.js
+++ db/seeds/seedModels.js/SeedBudgetModel.js
@@ -1,22 +1,21 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 const CategorySpendsSchema = new mongoose.Schema({
   categoryId: { type: Number, required: true },
   amount: { type: Number, required: true },
-  transactionId: { type: mongoose.Types.ObjectId, ref: 'TransactionSchema' },
+  transactionId: { type: mongoose.Types.ObjectId, ref: "TransactionSchema" },
 });
 
 const CategoryIncomeSchema = new mongoose.Schema({
   categoryId: { type: Number, required: true },
   amount: { type: Number, required: true },
-  transactionId: { type: mongoose.Types.ObjectId, ref: 'TransactionSchema' },
+  transactionId: { type: mongoose.Types.ObjectId, ref: "TransactionSchema" },
 });
 
 const BudgetSchema = new mongoose.Schema({
-  user_id: { type: Number, required: true },
-  income_t_count: {type: Number, required: false},
-  expense_t_count: {type: Number, required: false},
+  user_id: { type: {}, required: true },
+  income_t_count: { type: Number, required: false },
+  expense_t_count: { type: Number, required: false },
   balance: { type: Number, required: true },
   total_income: { type: Number, required: true },
   total_expenses: { type: Number, required: true },
@@ -26,8 +25,6 @@ const BudgetSchema = new mongoose.Schema({
   updatedAt: { type: Date, default: Date.now },
 });
 
-const Budget = mongoose.model('Budget', BudgetSchema);
+const Budget = mongoose.model("Budget", BudgetSchema);
 
 module.exports = Budget;
-
-
